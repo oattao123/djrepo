dacdf6918f15948f7aff886330b6aa09eed24631
oattao123 <72804848+oattao123@users.noreply.github.com>
2026-02-26 11:21:40 +0700

    ready

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*=-6n!783+v9u)gqm7^9o+u4q0td0_=*8(c^tmkrn_w!7f(@%='
+# SECRET_KEY = 'django-insecure-*=-6n!783+v9u)gqm7^9o+u4q0td0_=*8(c^tmkrn_w!7f(@%='
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-*=-6n!783+v9u)gqm7^9o+u4q0td0_=*8(c^tmkrn_w!7f(@%=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -49,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -98,8 +103,18 @@ DATABASES = {
 }
 
 DATABASES['default'] = DATABASES['sqlite']
-DATABASES['default'] = DATABASES['postgres']
+DATABASES['default'] = dj_database_url.config(
+    default=os.environ.get('DATABASE_URL',  f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+    conn_max_age=600
+)
+# DATABASES['default'] = DATABASES['postgres']
 
+# DATABASES = {
+#     'default' : dj_database_url.config(
+#         default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+#         conn_max_age=600
+#     )
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -139,3 +154,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
+
+if not DEBUG:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
